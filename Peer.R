fcb574348407e8c9e420865ff5d60d504815b056
tamimjim <40337273+tamimjim@users.noreply.github.com>
2018-06-22 23:54:40 +0600

    Inversion Matrix
    The function inverses a matrix assuming it has an inverse

--- Peer.R
+++ Peer.R
@@ -6,35 +6,17 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
-CacheMatrix <- function(x = matrix()) {
-        inverse <- NULL
-        set <- function(y) {
-                x <<- y
-                inverse <<- NULL
-        }
-        get <- function() x
-        setInverse <- function(inverse) inverse <<- inverse
-        getInverse <- function() inverse
-        list(set = set,
-             get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)
-}
-
-
-## This function computes the inverse of the special "matrix" created by 
-## CacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then it should retrieve the inverse from the cache.
-
-Solve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inverse <- x$getInverse()
-        if (!is.null(inv)) {
-                message("getting cached data")
-                return(inverse)
-        }
-        mat <- x$get()
-        inverse <- solve(mat, ...)
-        x$setInverse(inverse)
-        inverse
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+            x <<- y
+            inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
